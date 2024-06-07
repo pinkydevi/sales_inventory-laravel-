8da53c1f17bc0769fb1efeba9fea4adf4c291143
Pinky Devi <pinkydevi456u@gmail.com>
2024-06-07 23:22:26 +0600

    sales inventory management

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Model
+{
+    protected $fillable = ['firstName','lastName','email','mobile','password','otp'];
+
+    protected $attributes = [
+        'otp'=>'0'
+    ];
+
+}
